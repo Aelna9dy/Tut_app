4d2b623d0edd31ac5a25251ad604e8b1e26a219a
Ahmed Elnagdy <aelnagdy84@gmail.com>
2025-08-24 19:00:48 +0300

    Create TutApp Create Presentation Layer (Static Color, Static font, Static value) add style_manager

---
+++ lib/presentation/style_manager.dart
@@ -0,0 +1,88 @@
+import 'dart:ui';
+
+import 'font_manager.dart';
+
+TextStyle _getTextStyle(
+  double fontSize,
+  String fontFamily,
+  FontWeight fontWeight,
+  Color color,
+) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    fontWeight: fontWeight,
+    color: color,
+  );
+}
+
+// Regular Style
+
+TextStyle getRegularStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.regular,
+    color,
+  );
+}
+
+// Light Style
+
+TextStyle getLightStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.light,
+    color,
+  );
+}
+
+// medium Style
+
+TextStyle getMediumStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.medium,
+    color,
+  );
+}
+
+// SemiBold Style
+
+TextStyle getSemiBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.semiBold,
+    color,
+  );
+}
+
+// Bold Style
+
+TextStyle getBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.bold,
+    color,
+  );
+}
+
