9bc9feb364235777c76d91d7aaae5b47fa005f4f
Ahmed Elnagdy <aelnagdy84@gmail.com>
2025-08-24 20:36:21 +0300

    Create TutApp Create Presentation Layer (Static Color, Static font, Static value, add style_manager, create Theme Data , Edit about Presentation Layer,)

---
+++ lib/presentation/theme_manager.dart
@@ -0,0 +1,71 @@
+import 'package:advanced_flutter/presentation/color_manager.dart';
+import 'package:advanced_flutter/presentation/font_manager.dart';
+import 'package:advanced_flutter/presentation/style_manager.dart';
+import 'package:advanced_flutter/presentation/value_manager.dart';
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    // main color of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.primaryOpacity70,
+    colorScheme: ColorScheme.light(
+      primary: ColorManager.primary,
+      secondary: ColorManager.grey, // هنا تحط بديل accentColor
+    ),
+
+    // card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+
+    // app bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+        color: ColorManager.white,
+        fontSize: FontSize.s16,
+      ),
+    ),
+
+    // Button theme
+    buttonTheme: ButtonThemeData(
+      shape: StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.primaryOpacity70,
+    ),
+
+    // elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(color: ColorManager.white,),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+      ),
+    ),
+
+    // Text theme
+    textTheme: TextTheme(
+      titleLarge:getSemiBoldStyle(color: ColorManager.darkGrey,fontSize: FontSize.s16,),
+      bodyLarge: getMediumStyle(color: ColorManager.lightGrey,fontSize: FontSize.s14),
+      bodyMedium: getRegularStyle(color: ColorManager.grey1,),
+      bodySmall: getRegularStyle(color: ColorManager.grey),
+    ),
+
+
+    //input decoration theme
+
+
+
+  );
+}
